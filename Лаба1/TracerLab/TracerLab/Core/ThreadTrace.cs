d582f70e9f3cbf399dab464375bbdffc51e296e2
Nikita <nickpelmog2003@gmail.com>
2022-09-11 23:33:27 +0300

    feat: add TracerMethod,ThreadTrace

--- Лаба1/TracerLab/TracerLab/Core/ThreadTrace.cs
+++ Лаба1/TracerLab/TracerLab/Core/ThreadTrace.cs
@@ -1,10 +1,46 @@
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
+using TracerLab.Core;
+
+[XmlType("thread")]
 public class ThreadTrace
 {
-    //todo it will represent thread trace result
-    private int threadId;
+    [DataMember][JsonProperty, XmlAttribute("id")] public int ThreadId { get; set; }
+    [DataMember][JsonProperty, XmlAttribute("time")] public long ThreadTime { get; set; }
+    [DataMember][JsonProperty, XmlElement("methods")] public List<MethodInfo> MethodInfo { get; set; }
 
     public ThreadTrace(int threadId)
     {
-        this.threadId = threadId;
+        ThreadId = threadId;
+        MethodInfo = new List<MethodInfo>();
     }
+    public ThreadTrace() { }
+
+    public void PushMethod(string methodName, string className, string allMethodPath)
+    {
+        MethodInfo.Add(new MethodInfo(methodName, className, allMethodPath));
+    }
+
+    public void PopMethod(string allMethodPath)
+    {
+        var index = MethodInfo.FindLastIndex(item => item.GetAllMethodPath() == allMethodPath);
+
+        if (index != MethodInfo.Count - 1)
+        {
+            var size = MethodInfo.Count - index - 1;
+            var childMethods = MethodInfo.GetRange(index + 1, size);
+
+            for (var i = 0; i < size; i++)
+                MethodInfo.RemoveAt(MethodInfo.Count - 1);
+
+            MethodInfo[index].SetChildMethods(childMethods);
+            MethodInfo[index].CalculateTime();
+        }
+
+        ThreadTime += MethodInfo[index].GetTime();
+        MethodInfo[index].CalculateTime();
+    }
+
 }
---
+++ Лаба1/TracerLab/TracerLab/Core/TracerMethod.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.Serialization;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
+
+namespace TracerLab.Core
+{
+    public class MethodInfo
+    {
+        [JsonProperty, XmlAttribute("name")] public string MethodName { get; set; }
+        [JsonProperty, XmlAttribute("class")] public string ClassName { get; set; }
+        [JsonProperty, XmlAttribute("time")] public double Time { get; set; }
+
+        [JsonProperty, XmlElement("methods")]
+        //[JsonIgnore]
+        public List<MethodInfo> ChildMethods { get; set; }
+
+        [JsonIgnore]
+        private readonly string _allMethodPath;
+
+        [JsonIgnore] private readonly Stopwatch _stopwatch = new Stopwatch();
+
+        public MethodInfo(string methodName, string className, string allMethodPath)
+        {
+            MethodName = methodName;
+            ClassName = className;
+            _allMethodPath = allMethodPath;
+            _stopwatch.Start();
+
+        }
+        public MethodInfo() { }
+
+        public void SetChildMethods(List<MethodInfo> childMethods)
+        {
+            ChildMethods = childMethods;
+        }
+        public string GetAllMethodPath()
+        {
+            return _allMethodPath;
+        }
+
+        public void CalculateTime()
+        {
+            _stopwatch.Stop();
+            Time = _stopwatch.ElapsedMilliseconds;
+        }
+
+        public long GetTime()
+        {
+            return _stopwatch.ElapsedMilliseconds;
+        }
+    }
+}
