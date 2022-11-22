3c00bb3cd1b65dd1159f87af8b8d23461add9b6a
Nikita <nickpelmog2003@gmail.com>
2022-11-23 00:25:22 +0300

    feat: add test classes

---
+++ Lab2/MainApp/MainApp/TestStructures/BrokenHeartClub.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainApp.TestStructures
+{
+    public struct BrokenHeartClub
+    {
+        public int NumberOfMembers { get; set; }
+        public int NumberOfHearts { get; set; }
+
+        public BrokenHeartClub(int numberOfSeats, int numberOfHearts)
+        {
+            NumberOfMembers = numberOfSeats;
+            NumberOfHearts = numberOfHearts;
+        }
+    }
+}
