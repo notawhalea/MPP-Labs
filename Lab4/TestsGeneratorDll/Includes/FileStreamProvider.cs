bd186f627fcb1e7cf77c600abef47566a3469989
Nikita <nickpelmog2003@gmail.com>
2022-12-02 01:00:08 +0300

    feat: add providers and logic for test generator

---
+++ Lab4/TestsGeneratorDll/Includes/FileStreamProvider.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TestsGeneratorDll.Includes
+{
+    internal class FileStreamProvider
+    {
+        object syn = new object();
+        int activeTasks = 0;
+        int restriction;
+
+        public void SaveTestsIntoTheFiles(List<string> xUnitTests, string savePath, int restriction)
+        {
+            this.restriction = restriction;
+
+            int counter = 1;
+            foreach (var xUnitTest in xUnitTests)
+            {
+                lock (syn)
+                    if (activeTasks == restriction)
+                        Monitor.Wait(syn);
+
+                Task asyncTask = WriteFileAsync(savePath, $"Test_TheNightWeMet{counter}.cs", xUnitTest);
+                counter++;
+            }
+
+            lock (syn)
+                if (activeTasks != 0)
+                    Monitor.Wait(syn);
+        }
+
+        private async Task WriteFileAsync(string savePath, string file, string content)
+        {
+            lock (syn)
+                activeTasks += 1;
+
+            using (StreamWriter outputFile = new StreamWriter(Path.Combine(savePath, file)))
+            {
+
+                await outputFile.WriteAsync(content);
+            }
+
+            lock (syn)
+            {
+                activeTasks--;
+
+                if (activeTasks == restriction - 1)
+                    Monitor.Pulse(syn);
+
+                if (activeTasks == 0)
+                    Monitor.Pulse(syn);
+            }
+        }
+    }
+}
---
+++ Lab4/TestsGeneratorDll/Includes/MethodsProvider.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Threading;
+
+namespace TestsGeneratorDll.Includes
+{
+    internal class MethodsProvider
+    {
+        object syn = new object();
+
+        int numberOfTasksToComplete;    // количество задач которые необходимо выполнить
+        List<MethodInfo> methodInfos = new List<MethodInfo>();
+
+        public List<MethodInfo> GetMethodsInfo(List<Assembly> assemblies)
+        {
+            numberOfTasksToComplete = assemblies.Count;
+
+            foreach (var assembly in assemblies)
+            {
+                ThreadPool.QueueUserWorkItem(GetTestMethodsFromASingleAssembly, assembly);
+            }
+
+            lock (syn)
+                if (numberOfTasksToComplete > 0)
+                    Monitor.Wait(syn);
+
+            return methodInfos;
+        }
+
+        private void GetTestMethodsFromASingleAssembly(object assemblyObj)
+        {
+            var assembly = (Assembly)assemblyObj;
+
+            //получить все public Типы доступные в сборке (т.е. получить все public классы)
+            Type[] types = assembly.GetExportedTypes();
+
+            foreach (Type type in types)
+            {
+                //DeclaredOnly - Указывает, что следует учитывать только члены, объявленные на уровне иерархии предоставленного типа. Унаследованные члены не учитываются.
+                MethodInfo[] methods = type.GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.DeclaredOnly | BindingFlags.Instance);
+                foreach (MethodInfo method in methods)
+                {
+                    lock (syn)
+                        methodInfos.Add(method);
+                }
+            }
+
+            lock (syn)
+            {
+                numberOfTasksToComplete--;
+
+                if(numberOfTasksToComplete == 0)
+                    Monitor.Pulse(syn);
+            }
+        }
+    }
+}
